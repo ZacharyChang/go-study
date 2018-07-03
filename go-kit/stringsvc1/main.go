82499791eed3e5baafcf09005574642bb15ff494
Zachary <zacharychang.zc@gmail.com>
2018-07-03 14:02:08 +0800

    update: add application instrumentation

--- go-kit/stringsvc1/main.go
+++ go-kit/stringsvc1/main.go
@@ -5,15 +5,40 @@ import (
 	"os"
 
 	"github.com/go-kit/kit/log"
+	kitprometheus "github.com/go-kit/kit/metrics/prometheus"
 	httptransport "github.com/go-kit/kit/transport/http"
+	stdprometheus "github.com/prometheus/client_golang/prometheus"
 )
 
 func main() {
 	logger := log.NewLogfmtLogger(os.Stderr)
 
+	fieldKeys := []string{
+		"method", "error",
+	}
+	requestCount := kitprometheus.NewCounterFrom(stdprometheus.CounterOpts{
+		Namespace: "my_group",
+		Subsystem: "string_service",
+		Name:      "request_count",
+		Help:      "Number of requests received",
+	}, fieldKeys)
+	requestLatency := kitprometheus.NewSummaryFrom(stdprometheus.SummaryOpts{
+		Namespace: "my_group",
+		Subsystem: "string_service",
+		Name:      "request_latency_microseconds",
+		Help:      "Total duration of requests in microseconds.",
+	}, fieldKeys)
+	countResult := kitprometheus.NewSummaryFrom(stdprometheus.SummaryOpts{
+		Namespace: "my_group",
+		Subsystem: "string_service",
+		Name:      "count_result",
+		Help:      "The result of each count method.",
+	}, []string{})
+
 	var svc StringService
 	svc = stringService{}
 	svc = loggingMiddleware{logger, svc}
+	svc = instrumentingMiddleware{requestCount, requestLatency, countResult, svc}
 
 	uppercase := makeUppercaseEndpoint(svc)
 	uppercaseHandler := httptransport.NewServer(
@@ -30,5 +55,7 @@ func main() {
 	)
 	http.Handle("/uppercase", uppercaseHandler)
 	http.Handle("/count", countHandler)
-	http.ListenAndServe(":8080", nil)
+	http.Handle("/metrics", stdprometheus.Handler())
+	logger.Log("msg", "HTTP", "addr", ":8080")
+	logger.Log("err", http.ListenAndServe(":8080", nil))
 }
--- go-kit/stringsvc1/middleware.go
+++ go-kit/stringsvc1/middleware.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"time"
 
 	"github.com/go-kit/kit/log"
+	"github.com/go-kit/kit/metrics"
 
 	"github.com/go-kit/kit/endpoint"
 )
@@ -14,6 +16,13 @@ type Middleware func(endpoint.Endpoint) endpoint.Endpoint
 type loggingMiddleware struct {
 	logger log.Logger
 	next   StringService
+}
+
+type instrumentingMiddleware struct {
+	requestCount   metrics.Counter
+	requestLatency metrics.Histogram
+	countResult    metrics.Histogram
+	next           StringService
 }
 
 func (mw loggingMiddleware) Uppercase(ctx context.Context, s string) (output string, err error) {
@@ -38,6 +47,33 @@ func (mw loggingMiddleware) Count(ctx context.Context, s string) (n int) {
 			"n", n,
 			"took", time.Since(begin),
 		)
+	}(time.Now())
+	n = mw.next.Count(ctx, s)
+	return
+}
+
+func (mw instrumentingMiddleware) Uppercase(ctx context.Context, s string) (output string, err error) {
+	defer func(begin time.Time) {
+		lvs := []string{
+			"method", "uppercase",
+			"error", fmt.Sprint(err != nil),
+		}
+		mw.requestCount.With(lvs...).Add(1)
+		mw.requestLatency.With(lvs...).Observe(time.Since(begin).Seconds())
+	}(time.Now())
+	output, err = mw.next.Uppercase(ctx, s)
+	return
+}
+
+func (mw instrumentingMiddleware) Count(ctx context.Context, s string) (n int) {
+	defer func(begin time.Time) {
+		lvs := []string{
+			"method", "count",
+			"error", "false",
+		}
+		mw.requestCount.With(lvs...).Add(1)
+		mw.requestLatency.With(lvs...).Observe(time.Since(begin).Seconds())
+		mw.countResult.Observe(float64(n))
 	}(time.Now())
 	n = mw.next.Count(ctx, s)
 	return
