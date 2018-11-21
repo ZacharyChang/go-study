46d2dcc570354fc75c495e7ac7948cfcf7061ab9
Zachary <zacharychang.zc@gmail.com>
2018-11-21 21:10:27 +0800

    example: #threaded_ip_echo_server

---
+++ tcp/threaded_ip_echo_server.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+)
+
+func main() {
+	svc := ":1208"
+
+	listener, err := net.Listen("tcp", svc)
+	checkErr(err)
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			continue
+		}
+		go handleClient(conn)
+	}
+}
+
+func handleClient(conn net.Conn) {
+	// close connection on exit
+	defer conn.Close()
+
+	var buf [512]byte
+	for {
+		n, err := conn.Read(buf[0:])
+		if err != nil {
+			return
+		}
+
+		_, err2 := conn.Write(buf[0:n])
+		if err2 != nil {
+			return
+		}
+	}
+}
+
+func checkErr(err error) {
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
+		os.Exit(1)
+	}
+}
